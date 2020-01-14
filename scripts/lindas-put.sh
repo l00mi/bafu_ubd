a8aaa5a8d65166bc247fa62e0f018817cb1b81ce
Adrian Gschwend <ktk@netlabs.org>
2020-01-14 16:00:17 +0100

    Adjust for new LINDAS endpoint

--- scripts/lindas-put.sh
+++ scripts/lindas-put.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 curl -n \
      -X PUT \
+     --http1.1 \
      -H Content-Type:application/n-triples \
      -T target/ubd28.nt \
-     -G https://lindas-data.ch:8443/lindas \
+     -G https://stardog-test.cluster.ldbar.ch/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/FOEN/UBD28
 curl -n \
      -X PUT \
+     --http1.1 \
      -H Content-Type:application/n-triples \
      -T target/ubd66.nt \
-     -G https://lindas-data.ch:8443/lindas \
+     -G https://stardog-test.cluster.ldbar.ch/lindas \
      --data-urlencode graph=https://linked.opendata.swiss/graph/FOEN/UBD66
